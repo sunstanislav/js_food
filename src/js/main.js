f6887c59a9df521e620b675fa2811b3c33bde565
sunstanislav <sundeev91@ya.ru>
2022-02-17 20:55:51 +0600

    modal fix

--- src/js/main.js
+++ src/js/main.js
@@ -96,16 +96,30 @@ window.addEventListener('DOMContentLoaded', () => {
         modalCloseBtn = document.querySelector('[data-close]');
 
         modalTrigger.forEach(btn => {
-            btn.addEventListener('click', () => {
+            btn.addEventListener('click', function() {
                 modal.classList.add('show');
                 modal.classList.remove('hide');
                 document.body.style.overflow = 'hidden';
             });
         });
 
-        modalCloseBtn.addEventListener('click', () => {
-            modal.classList.remove('show');
+        function closeModal() {
             modal.classList.add('hide');
+            modal.classList.remove('show');
             document.body.style.overflow = '';
+        }
+
+        modalCloseBtn.addEventListener('click', closeModal);
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                closeModal();
+            }
+        });
+        
+        document.addEventListener('keydown', (e) => {
+            if (e.code === "Escape" && modal.classList.contains('show')) { 
+                closeModal();
+            }
         });
 });
