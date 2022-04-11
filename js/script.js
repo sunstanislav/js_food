9ff7d081806a3eef1043d4097aae095c5ea26df1
sunstanislav <sundeev91@ya.ru>
2022-04-11 20:47:32 +0600

    Update 2022-04-11T14:47:27.492Z

--- js/script.js
+++ js/script.js
@@ -86,12 +86,75 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./node_modules/jshint/src/prod-params.js":
+/*!************************************************!*\
+  !*** ./node_modules/jshint/src/prod-params.js ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/**
+ * This module defines a set of enum-like values intended for use as bit
+ * "flags" during parsing. The ECMAScript grammar defines a number of such
+ * "production parameters" to control how certain forms are parsed in context.
+ * JSHint implements additional parameters to facilitate detection of lint
+ * warnings.
+ *
+ * An equivalent implementation which described the context in terms of a
+ * "lookup table" object would be more idiomatic for a JavaScript project like
+ * JSHint. However, because the number of contexts scales with the number of
+ * expressions in the input program, this would have non-negligible impact on
+ * the process's memory footprint.
+ */
+module.exports = {
+  /**
+   * Enabled when parsing expressions within ES2015 "export" declarations,
+   * allowing otherwise-unreferenced bindings to be considered "used".
+   */
+  export: 1,
+
+  /**
+   * Enabled when parsing expressions within the head of `for` statements,
+   * allowing to distinguish between `for-in` and "C-style" `for` statements.
+   */
+  noin: 2,
+
+  /**
+   * Enabled when the expression begins the statement, allowing the parser to
+   * correctly select between the null denotation ("nud") and first null
+   * denotation ("fud") parsing strategy.
+   */
+  initial: 4,
+
+  preAsync: 8,
+
+  async: 16,
+
+  /**
+   * Enabled when any exception thrown by the expression will be caught by a
+   * TryStatement.
+   */
+  tryClause: 32,
+
+  /**
+   * Enabled when parsing the body of a generator function.
+   */
+  yield: 64
+};
+
+
+/***/ }),
+
 /***/ "./src/js/main.js":
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+const {
+  async
+} = __webpack_require__(/*! jshint/src/prod-params */ "./node_modules/jshint/src/prod-params.js");
 
 window.addEventListener('DOMContentLoaded', function () {
   // Tabs
@@ -109,7 +172,8 @@ window.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-  function showTabContent(i = 0) {
+  function showTabContent() {
+    let i = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
     tabsContent[i].classList.add('show', 'fade');
     tabsContent[i].classList.remove('hide');
     tabs[i].classList.add('tabheader__item_active');
@@ -130,7 +194,7 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   }); // Timer
 
-  const deadline = '2020-05-11';
+  const deadline = '2022-08-23';
 
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -220,12 +284,17 @@ window.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('scroll', showModalByScroll); // Используем классы для создание карточек меню
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+    constructor(src, alt, title, descr, price, parentSelector) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+
+      for (var _len = arguments.length, classes = new Array(_len > 6 ? _len - 6 : 0), _key = 6; _key < _len; _key++) {
+        classes[_key - 6] = arguments[_key];
+      }
+
       this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
@@ -261,9 +330,46 @@ window.addEventListener('DOMContentLoaded', function () {
 
   }
 
-  new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 9, ".menu .container").render();
-  new MenuCard("img/tabs/post.jpg", "post", 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 14, ".menu .container").render();
-  new MenuCard("img/tabs/elite.jpg", "elite", 'Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 21, ".menu .container").render(); // Forms
+  const getResource = async url => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getResource('http://localhost:3000/menu').then(data => {
+    data.forEach(_ref => {
+      let {
+        img,
+        altimg,
+        title,
+        descr,
+        price
+      } = _ref;
+      new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    });
+  }); // getResource('http://localhost:3000/menu')
+  //     .then(data => createCard(data));
+  // function createCard(data) {
+  //     data.forEach(({img, altimg, title, descr, price}) => {
+  //         const element = document.createElement('div');
+  //         element.classList.add('menu__item');
+  //         element.innerHTML = `
+  //         <img src=${img} alt=${altimg}>
+  //         <h3 class="menu__item-subtitle">${title}</h3>
+  //         <div class="menu__item-descr">${descr}</div>
+  //         <div class="menu__item-divider"></div>
+  //         <div class="menu__item-price">
+  //             <div class="menu__item-cost">Цена:</div>
+  //             <div class="menu__item-total"><span>${price}</span> грн/день</div>
+  //         </div>
+  //         `;
+  //         document.querySelector('.menu .container').append(element);
+  // });
+  // Forms
 
   const forms = document.querySelectorAll('form');
   const message = {
@@ -272,10 +378,21 @@ window.addEventListener('DOMContentLoaded', function () {
     failure: 'Что-то пошло не так...'
   };
   forms.forEach(item => {
-    postData(item);
+    BindPostData(item);
   });
 
-  function postData(form) {
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data
+    });
+    return await res.json();
+  };
+
+  function BindPostData(form) {
     form.addEventListener('submit', e => {
       e.preventDefault();
       let statusMessage = document.createElement('img');
@@ -286,17 +403,8 @@ window.addEventListener('DOMContentLoaded', function () {
             `;
       form.insertAdjacentElement('afterend', statusMessage);
       const formData = new FormData(form);
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
-      fetch('server.php', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(object)
-      }).then(data => {
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
+      postData('http://localhost:3000/requests', json).then(data => {
         console.log(data);
         showThanksModal(message.success);
         statusMessage.remove();
@@ -328,6 +436,8 @@ window.addEventListener('DOMContentLoaded', function () {
       closeModal();
     }, 4000);
   }
+
+  fetch('http://localhost:3000/menu').then(data => data.json()).then(res => console.log(res));
 });
 
 /***/ })
